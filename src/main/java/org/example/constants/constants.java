a72b09a035103036cac966fe80c77db468da4726
ClientApi <de_ny@i.ua>
2023-05-05 02:20:45 +0300

    New master?

---
+++ src/main/java/org/example/constants/constants.java
@@ -0,0 +1,11 @@
+package org.example.constants;
+//master
+public class constants {
+   // popup_make_del
+    public static final String STATIC_PROJECT ="https://login.sendpulse.com/pop-ups/project/ceb77478-19c6-4d70-b1a2-c3b28a9abd72/";
+    public static final String NAME_OF_NEW_PROJECT = "https://auto.html";
+    //public static final String ENVIRONMENT ="https://pre.sendpulse.com/login/";  //прпепрод
+    public static final String ENVIRONMENT ="https://login.sendpulse.com/login/";   //прод
+    public static final String PASS = "123456";
+    public static final String LOGIN = "d.valko+prod_auto@sendpulse.com";
+}
---
+++ src/test/java/LoginPage.java
@@ -0,0 +1,21 @@
+import com.codeborne.selenide.SelenideElement;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static org.example.constants.constants.*;
+
+
+public class LoginPage {
+    private final SelenideElement LOGIN_INPUT = $(By.cssSelector("#login"));
+    private final SelenideElement PASS_INPUT = $(By.cssSelector("#password"));
+    private final SelenideElement SUBMIT_BTN = $(By.cssSelector("button[name='submit']"));
+
+
+    public void login() {
+        open(ENVIRONMENT);
+        LOGIN_INPUT.sendKeys(LOGIN);
+        PASS_INPUT.sendKeys(PASS);
+        SUBMIT_BTN.click();
+    }
+}
